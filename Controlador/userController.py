b8f92f2506bce339e5e2ed9f5c4f0c51e19f2cea
Kevin Huayllas <KevinOmega01@gmail.com>
2024-11-13 18:01:28 -0400

    feat: funcionalidad para borrar usuarios de postgres

--- Controlador/userController.py
+++ Controlador/userController.py
@@ -69,6 +69,43 @@ def deleteUser(user):
     # reiniciar apache
     restartApache()
 
+    # conectar a la base de datos como postgres
+    subprocess.run(['sudo', 'service', 'postgresql', 'start'], check=True)
+    conn = psycopg2.connect(dbname="postgres", user="postgres",password="postgres")
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    # eliminar de base de datos con nombre del usuario si existe
+    try:
+        cur.execute(sql.SQL("DROP DATABASE IF EXISTS {}").format(sql.Identifier(user)))
+        print(f"Base de datos '{user}' eliminada.")
+    # eliminar usuario con nombre del usuario si existe
+        cur.execute(sql.SQL("DROP USER IF EXISTS {}").format(sql.Identifier(user)))
+        print(f"Usuario '{user}' eliminado.")
+    except:
+        pass
+
+    # cerrar conexion
+    cur.close()
+    conn.close()
+
+    #borrar de el archivo pg_hba.conf
+    hba_path = "/var/lib/pgsql/data/pg_hba.conf"
+    with open(hba_path, "r") as f:
+        pg_hba = f.readlines()
+    index = int()
+    for i in range(0,len(pg_hba)):
+        if f"local   {user}" in pg_hba[i]:
+            index = i
+            break
+    if index == 0:
+        print("User not found")
+    else:    
+        del pg_hba[index]
+        with open("/etc/postgresql/12/main/pg_hba.conf", "w") as f:
+        f.writelines(pg_hba)
+        print("Usuario borrado del archivo pg_hba.conf")
+
     # return
     return {
         "status": 200,
