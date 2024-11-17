68619f01419e4e2a18c13c74f5b4227d33e8c780
Kevin Huayllas <KevinOmega01@gmail.com>
2024-11-17 12:42:24 -0400

    feat: funcion para realizar la configuracion inicial de ftp

---
+++ FTP/ftp.py
@@ -0,0 +1,67 @@
+
+
+def conf_ftp():
+    vsftpdFile = '/etc/vsftpd.conf'
+
+    try:
+        # Leer el archivo línea por línea
+        with open(vsftpdFile, "r") as f:
+            vsftpd = f.readlines()
+
+        # Buscar la línea de referencia y realizar los reemplazos
+        index_local_enable = -1
+        index_write_enable = -1
+        index_chroot_local_user = -1
+        index_chroot_list_enable = -1
+        index_chroot_list_enable_2 = -1
+        index_chroot_list_file = -1
+        index_allow_writeable_enable = -1
+        for i in range(len(vsftpd)):
+            if "local_enable=NO" in vsftpd[i]:
+                index_local_enable = i
+            if "write_enable=NO" in vsftpd[i]:
+                index_write_enable = i
+            if "#chroot_local_user=YES" in vsftpd[i]:
+                index_chroot_local_user = i
+            if "#chroot_list_enable=YES" in vsftpd[i]:
+                index_chroot_list_enable = i
+            if "chroot_list_enable=YES" in vsftpd[i]:
+                index_chroot_list_enable_2 = i
+            if "allow_writeable_chroot=YES" in vsftpd[i]:
+                index_allow_writeable_enable = i
+            if "#chroot_list_file=/etc/vsftpd.chroot_list" in vsftpd[i]:
+                index_chroot_list_file = i
+                break
+
+        if index_local_enable == -1:
+            print("Línea no encontrada.")
+            return
+
+        # Reemplazar las líneas después de la línea encontrada
+        print(index_allow_writeable_enable,"we")
+        if index_local_enable != -1:
+            vsftpd[index_local_enable] = "local_enable=YES\n"
+        if index_write_enable != -1:
+            vsftpd[index_write_enable] = "write_enable=YES\n"
+        if index_chroot_local_user != -1:
+            vsftpd[index_chroot_local_user] = "chroot_local_user=YES\n"
+        if index_chroot_list_enable != -1:
+            vsftpd[index_chroot_list_enable] = "chroot_list_enable=YES\n"
+            if index_allow_writeable_enable == -1:
+                vsftpd[index_chroot_list_enable] = vsftpd[index_chroot_list_enable] + "allow_writeable_chroot=YES\n"
+        elif index_chroot_list_enable_2 != -1:
+            if index_allow_writeable_enable == -1:
+                vsftpd[index_chroot_list_enable_2] = vsftpd[index_chroot_list_enable_2] + "allow_writeable_chroot=YES\n"
+        
+        if index_chroot_list_file != -1:
+            vsftpd[index_chroot_list_file] = "chroot_list_file=/etc/vsftpd.chroot_list\n"
+
+        # Escribir los cambios en el archivo
+        with open(vsftpdFile, "w") as f:
+            f.writelines(vsftpd)
+
+        print("Líneas reemplazadas correctamente en el archivo vsftpd.conf.")
+    except FileNotFoundError:
+        print(f"Archivo no encontrado: {vsftpdFile}")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
